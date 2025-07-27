2f26f12a6de1e9f81ce39e7a8c7562458aa9d445
jacobkhajjar <jacob.k.hajjar@gmail.com>
2025-07-27 10:22:15 +0100

    homonym loop

--- main.py
+++ main.py
@@ -2,34 +2,43 @@ import cmudict
 from objects.consonants import classify_consonant
 from objects.vowels import Vowel, vowel_list, classify_vowel
 
-word = "enough"
+word = "object"
 cmu = cmudict.dict()
 
 
 def main():
-    phones = []
     lexical_sets = set()
     tokens = cmu[word]
+    i = 0
 
-    print(f'lexiguessing "{word}"')
-    print(f"dictionary glyphs: {tokens}")
+    print(f'lexiguessing "{word}"\n')
 
-#need to add cases for homonyms and words not in dictionary and symbol not found
+#need to add cases for words not in dictionary and symbol not found
 
-    for token in tokens[0]:
-        if token[0] in vowel_list:
-            token = classify_vowel(token)
-        else:
-            token = classify_consonant(token)
+    while i < len(tokens):
+        print(f"dictionary glyphs: {tokens[i]}")
         
-        phones.append(token)
+        phones = []
 
-    transcription = ""
-    for phone in phones:
-        fauxnetic = phone.fx
-        if isinstance(phone, Vowel) and not phone.is_stressed:
-            fauxnetic = fauxnetic.lower()
-        transcription += fauxnetic + " "
-    print(f"fauxnetic transcription: {transcription}")
+        for token in tokens[i]:
+            if token[0] in vowel_list:
+                token = classify_vowel(token)
+            else:
+                token = classify_consonant(token)
+        
+            phones.append(token)
+
+        transcription = ""
+        for phone in phones:
+            fauxnetic = phone.fx
+            if isinstance(phone, Vowel) and not phone.is_stressed:
+                fauxnetic = fauxnetic.lower()
+            transcription += fauxnetic + " "
+        print(f"fauxnetic transcription: {transcription}")
+        
+        if i != len(tokens) - 1:
+            print("\n~~~ homonym found, running again ~~~\n")
+
+        i += 1
 
 main()
